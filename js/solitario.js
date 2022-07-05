abf48621c0e6151527ae1e9656e540896af04b85
vmartinez33 <vmartineza33@gmail.com>
2022-07-05 20:14:50 +0200

    Archivos cambiados por el profesor, habian nombres de variables que no cuadraban

--- js/solitario.js
+++ js/solitario.js
@@ -27,16 +27,16 @@ let mazo_receptor3 = [];
 let mazo_receptor4 = [];
 
 // Contadores de cartas
-let cont_inicial     = document.getElementById("cont_inicial");
-let cont_sobrantes   = document.getElementById("cont_sobrantes");
-let cont_receptor1   = document.getElementById("cont_receptor1");
-let cont_receptor2   = document.getElementById("cont_receptor2");
-let cont_receptor3   = document.getElementById("cont_receptor3");
-let cont_receptor4   = document.getElementById("cont_receptor4");
-let cont_movimientos = document.getElementById("cont_movimientos");
+let cont_inicial     = document.getElementById("contador_inicial");
+let cont_sobrantes   = document.getElementById("contador_sobrantes");
+let cont_receptor1   = document.getElementById("contador_receptor1");
+let cont_receptor2   = document.getElementById("contador_receptor2");
+let cont_receptor3   = document.getElementById("contador_receptor3");
+let cont_receptor4   = document.getElementById("contador_receptor4");
+let cont_movimientos = document.getElementById("contador_movimientos");
 
 // Tiempo
-let cont_tiempo  = document.getElementById("cont_tiempo"); // span cuenta tiempo
+let cont_tiempo  = document.getElementById("contador_tiempo"); // span cuenta tiempo
 let segundos 	 = 0;    // cuenta de segundos
 let temporizador = null; // manejador del temporizador
 
